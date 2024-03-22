cebe9e9577a39be98a2f69cf164f432767372cee
Lai Yi Ohlsen <laiyi.ohlsen@gmail.com>
2024-03-22 10:42:12 -0400

    adding week9

---
+++ week9/button_example/script.js
@@ -0,0 +1,81 @@
+/* ------------------------------------------------
+// EVENT HANDLER FORMAT
+
+Element.addEventListener(event, function){
+*action that you want to take place*
+}
+
+// ARROW FUNCTION FORMAT
+element.addEventListener("click", () => {
+
+})
+------------------------------------------------*/
+
+// ADD EVENT HANDLER
+
+let btn = document.querySelector("button"); 
+
+btn.addEventListener("click", () => {
+
+/* ------------------------------------------------   
+
+// PART ONE - change background color when button is pressed
+
+    let x = Math.floor(Math.random() * 255); 
+    let y = Math.floor(Math.random() * 255); 
+    let z = Math.floor(Math.random() * 255); 
+
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")"
+    console.log(bgColor)
+
+    document.body.style.background = bgColor; 
+    ------------------------------------------------*/
+
+    /* ------------------------------------------------
+    // PART THREE - replace headers with new words
+
+    let frm1 = document.getElementById ("frm1"); 
+
+    let h2s = document.getElementsByTagName("H2");
+
+    console.log(frm1);
+    console.log(frm1.elements);
+    console.log(h2s);
+
+    h2s[0].innerHTML = frm1.elements[0].value; 
+    h2s[1].innerHTML = frm1.elements[1].value; 
+    ------------------------------------------------*/
+
+
+    /* ------------------------------------------------
+    // PART 4 - add headers instead of replacing 
+
+    let frm1 = document.getElementById ("frm1"); 
+
+    let h2s = document.getElementsByTagName("H2");
+
+    console.log(frm1);
+    console.log(frm1.elements);
+    console.log(h2s);
+
+    let i; 
+    let elem = []; 
+    for (i = 0; i < frm1.length; i++){ 
+    elem.push(document.createElement("h3")); 
+    elem[i].innerHTML = frm1.elements[i].value; 
+    document.body.appendChild(elem[i]);
+    }
+    ------------------------------------------------*/
+
+})
+
+/* ------------------------------------------------
+// PART TWO - remove text when clicked
+
+removeText.onclick = function() { 
+removeText.remove(); 
+console.log("remove")
+}
+------------------------------------------------*/
+
+
