456f7db9c51cf3c71f9a467d1f2ff3460c43bb07
Lai Yi Ohlsen <laiyi.ohlsen@gmail.com>
2024-03-22 11:08:20 -0400

    adding comments to todo list example

--- week9/todolist/script.js
+++ week9/todolist/script.js
@@ -2,15 +2,20 @@
 let btn = document.getElementById("submit")
 
 btn.addEventListener("click", () => {
+    // get input from form
     let todo = document.getElementById("form");
-    console.log(todo.elements[0].value);
+    console.log(todo.elements);
     
-
+    // create new list item 
     let newItem = document.createElement("li");
+    //set its value to the input
     newItem.innerHTML = todo.elements[0].value; 
     
+    // find the list in the DOM
     let list = document.getElementById("list");
-    // console.log(list)
+    // add the new list item to the list
     list.appendChild(newItem);
+    
+    //reset box
     todo.elements[0].value ="";
 })
